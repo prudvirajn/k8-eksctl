300301317efb19c272c86be0aa7b2f39073e955c
Rajprudvin <prudvirajn@gmail.com>
2024-11-07 13:59:20 +0530

    k8-resources

--- workstation.sh
+++ workstation.sh
@@ -43,7 +43,6 @@ fi
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
-eksctl version
 VALIDATE $? "eksctl installation"
 
 
